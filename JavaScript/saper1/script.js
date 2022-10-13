aa255b3cfd1034c4fd8f42a0ff595a7a69d9f7c7
MrJujek <mr.jujek@gmail.com>
2022-10-13 16:14:17 +0000

    saper

--- JavaScript/saper1/script.js
+++ JavaScript/saper1/script.js
@@ -2,24 +2,34 @@ let formularz = document.createElement("form");
 let input, label, height, width, mines
 let dane, box
 let opis = ["Height", "Width", "Mines"]
+let element, count
+let saperFreezeClic
 
 for (let i = 0; i < 3; i++) {
     input = document.createElement("input")
-    input.setAttribute("type", "text")
-    input.setAttribute("id", "input_" + opis[i])
+    input.setAttribute({
+        "type": "text",
+        "value": 5,
+        "id": "input_" + opis[i]
+    })
 
     label = document.createElement("label")
-    label.setAttribute("for", "input_" + opis[i])
-    label.setAttribute("id", "label_" + opis[i])
+    label.setAttribute({
+        "for": "input_" + opis[i],
+        "id": "label_" + opis[i]
+    })
     label.innerHTML = opis[i] + ":"
 
     formularz.appendChild(label)
     formularz.appendChild(input)
 }
+
 input = document.createElement("input")
-input.setAttribute("type", "button")
-input.setAttribute("id", "generuj")
-input.setAttribute("value", "GENERUJ")
+input.setAttribute({
+    "type": "button",
+    "id": "generuj",
+    "value": "GENERUJ"
+})
 formularz.appendChild(input)
 
 formularz.setAttribute("class", "flex-column")
@@ -30,13 +40,20 @@ dane.appendChild(formularz);
 
 document.body.appendChild(dane);
 
+let saper_div = document.createElement("div");
+saper_div.setAttribute("id", "saper")
+document.body.appendChild(saper_div);
+
 document.getElementById('generuj').onclick = () => {
+    saperFreezeClic = false
+    document.getElementById("saper").innerHTML = null
+
     height = parseInt(document.getElementById("input_" + opis[0]).value)
     width = parseInt(document.getElementById("input_" + opis[1]).value)
     mines = parseInt(document.getElementById("input_" + opis[2]).value)
 
     if (isNaN(height) || isNaN(width) || isNaN(mines) || mines > (height - 1) * (width - 1)) {
-        console.log("Zle dane")
+        //console.log("Zle dane")
         setTimeout(() => {
             document.getElementById("input_" + opis[0]).value = ""
             document.getElementById("input_" + opis[1]).value = ""
@@ -48,8 +65,9 @@ document.getElementById('generuj').onclick = () => {
 }
 
 function saper(height, width, mines) {
-    let saper = document.createElement("div");
-    saper.setAttribute("id", "saper")
+    console.log(height, width, mines)
+
+    let saper = document.getElementById("saper")
     saper.style.width = width * 50 + "px"
     saper.style.position = "relative"
 
@@ -63,19 +81,104 @@ function saper(height, width, mines) {
             box.style.left = j * 50 + "px"
             box.style.top = i * 50 + "px"
 
-
             saper.appendChild(box);
-
-            // document.getElementById("box_x" + i + "_y" + j).onclick = () => {
-            //     console.log("klik")
-            // }
         }
     }
 
-    document.getElementsByClassName("box").onclick = () => {
-        // console.log(this.id)
-        console.log("klik");
+    document.body.appendChild(saper);
+
+    saper.addEventListener("click", e => {
+        if (saperFreezeClic) {
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    }, true);
+    saper.addEventListener("contextmenu", e => {
+        if (saperFreezeClic) {
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    }, true);
+
+    let bomby = []
+    for (let i = 0; i < height; i++) {
+        bomby[i] = []
+        for (let j = 0; j < width; j++) {
+            let nazwa = "box_x" + i + "_y" + j
+            element = document.getElementById(nazwa)
+            element.style.backgroundImage = "url(./img/klepa.PNG)"
+
+            element.addEventListener("click", () => {
+                element = document.getElementById(nazwa)
+                if (bomby[i][j] == 9) {
+                    for (let i = 0; i < height; i++) {
+                        for (let j = 0; j < width; j++) {
+                            let nazwa_bomb = "box_x" + i + "_y" + j
+                            let bomb = document.getElementById(nazwa_bomb)
+                            if (bomby[i][j] == 9)
+                                bomb.style.backgroundImage = 'url("./img/pbomb.PNG")'
+                        }
+                    }
+
+                    element.style.backgroundImage = 'url("./img/bomb.PNG")'
+                    alert("przegrales")
+                    saperFreezeClic = true
+                } else {
+                    element.style.backgroundImage = ""
+                    element.innerText = bomby[i][j]
+                }
+                element.style.backgroundImage = 0
+                console.log(element.style.backgroundImage)
+                console.log("lewy klik: ", element)
+            })
+
+            element.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+                element = document.getElementById(nazwa)
+                console.log("prawy klik: ", element)
+
+                if (element.style.backgroundImage == 'url("./img/klepa.PNG")') {
+                    element.style.backgroundImage = 'url("./img/flaga.PNG")'
+                } else if (element.style.backgroundImage == 'url("./img/flaga.PNG")') {
+                    element.style.backgroundImage = 'url("./img/pyt.png")'
+                } else if (element.style.backgroundImage == 'url("./img/pyt.png")') {
+                    element.style.backgroundImage = 'url("./img/klepa.PNG")'
+                }
+            });
+
+
+        }
     }
 
-    document.body.appendChild(saper);
+    for (let k = 0; k < mines;) {
+        let x = Math.floor(Math.random() * width)
+        let y = Math.floor(Math.random() * height)
+        if (bomby[y][x] !== 9) {
+            bomby[y][x] = 9
+            k++
+        }
+    }
+
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (bomby[i][j] !== 9) {
+                count = 0
+
+                for (let k = i - 1; k <= i + 1; k++) {
+                    for (let l = j - 1; l <= j + 1; l++) {
+                        if (k >= 0 && l >= 0 && k < height && l < width) {
+                            if (bomby[k][l] === 9) {
+                                count++
+                            }
+                        }
+                    }
+                }
+
+                bomby[i][j] = count
+            }
+        }
+    }
+
+    console.log(bomby)
 }
