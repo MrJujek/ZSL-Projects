b46709c310d3a26006e556cc2f8ad6d2e287b1ae
MrJujek <mr.jujek@gmail.com>
2022-10-19 07:11:10 +0000

    flask

---
+++ Flask/06-Grades/app.py
@@ -0,0 +1,58 @@
+import json
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_bs4 import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'jv5f*&^FJHOij987gtyIG^R%$Dij78t%$D#$Afu'
+
+
+class LoginForm(FlaskForm):
+    """formularz logowania"""
+    userLogin = StringField("Nazwa użytkownika:", validators=[DataRequired()])
+    userPass = PasswordField("Hasło:", validators=[DataRequired()])
+    submit = SubmitField("Zaloguj")
+
+
+users = {1: {"userLogin": "julo", "userPass": "julo", "fname": "julo", "lname": "julo"}}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='strona glowna', userLogin=session.get("userLogin"))
+
+
+@app.route('/logIn', methods=["POST", "GET"])
+def logIn():
+    """obslugiwanie logowania uzytkownikow"""
+    login = LoginForm()
+    if login.validate_on_submit():
+        userLogin = login.userLogin.data
+        userPass = login.userPass.data
+        if userLogin == users[1]["userLogin"] and userPass == users[1]["userPass"]:
+            session["userLogin"] = userLogin
+            return redirect("dashboard")
+    return render_template('login.html', title='logowanie', login=login, userLogin=session.get("userLogin"))
+
+
+@app.route("/dashboard")
+def dashboard():
+    with open("data/grades.json") as gradesFile:
+        grades = json.load(gradesFile)
+        gradesFile.close()
+    return render_template("dashboard.html", title="dashboard", userLogin=session.get("userLogin"), grades = grades)
+
+@app.route("/logOut")
+def logOut():
+    session.pop("userLogin")
+    return redirect("logIn")
+
+if __name__ == '__main__':
+    app.run(debug=True)
