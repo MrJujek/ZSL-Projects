638b6b1e453c2284f0d92009d14fe047c6752a68
MrJujek <mr.jujek@gmail.com>
2022-11-10 11:32:27 +0000

    js + sql

--- JavaScript/lekcja7/script.js
+++ JavaScript/lekcja7/script.js
@@ -2,6 +2,7 @@ let wielkosc = 11
 let plansza = document.createElement("div")
 plansza.setAttribute("id", "plansza")
 plansza.style.position = "relative"
+let snake_length = 1
 
 for (let y = 0; y < wielkosc; y++) {
     for (let x = 0; x < wielkosc; x++) {
@@ -35,12 +36,59 @@ document.getElementById(nazwa).style.backgroundImage = "url(./krzak.png)"
 let pozycja = []
 
 pozycja.push({ x: random_x, y: random_y })
-show_snake(pozycja)
+document.onkeydown = (e) => {
+    console.log("klik");
+    let new_position
+    let new_x, new_y
+    let direction
+    switch (e.key) {
+        case "ArrowRight":
+            pozycja[0].x + 1 > wielkosc ? new_x = 0 : new_x = pozycja[0].x + 1
+            new_position = { x: new_x, y: pozycja[0].y }
+            pozycja.unshift(new_position)
+            console.log(pozycja);
+            break;
 
+        case "ArrowLeft":
+            pozycja[0].x - 1 < 0 ? new_x = wielkosc : new_x = pozycja[0].x - 1
+            new_position = { x: new_x, y: pozycja[0].y }
+            pozycja.unshift(new_position)
+            console.log(pozycja);
+            break;
 
-function show_snake(pozycja) {
+        case "ArrowDown":
+            pozycja[0].y + 1 > wielkosc ? new_y = 0 : new_y = pozycja[0].y + 1
+            new_position = { x: pozycja[0].x, y: new_y }
+            pozycja.unshift(new_position)
+            console.log(pozycja);
+            break;
+
+        case "ArrowUp":
+            pozycja[0].y - 1 < 0 ? new_y = wielkosc : new_y = pozycja[0].y - 1
+            new_position = { x: pozycja[0].x, y: new_y }
+            pozycja.unshift(new_position)
+            console.log(pozycja);
+            break;
+
+    }
+    console.log("po kliku");
+    if (pozycja.length > snake_length) {
+        pozycja.pop()
+    }
+    show_snake(pozycja, direction)
+}
+
+var snake_move
+function show_snake(pozycja, direction) {
+    clearInterval(snake_move)
+    snake_move = setInterval(() => {
+        console.log("asd");
+    }, 1000)
+
     for (let i = 0; i < pozycja.length; i++) {
+        console.log("asdasdasd");
         let nazwa = "pole_x" + pozycja[i].x + "_y" + pozycja[i].y
-        document.getElementById(nazwa).style.backgroundImage = "url(./krzak.png)"
+        let element = document.getElementById(nazwa)
+        element.style.backgroundColor = "red"
     }
 }
