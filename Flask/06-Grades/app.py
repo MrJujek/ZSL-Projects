33ecb82d12a8d65a02773a2789ab710274d8c6af
Julian Dworzycki <jdworzycki@sala514i.zsl>
2023-01-04 07:50:02 +0200

    flask

--- Flask/06-Grades/app.py
+++ Flask/06-Grades/app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_bs4 import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -8,53 +8,108 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
-
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 date = datetime.now()
-app.config['SECRET_KEY'] = 'jv5f*&^FJHOij987gtyIG^R%$Dij78t%$D#$Afu'
-
+app.config['SECRET_KEY'] = 'fghj456(&^$%^&*ghjG'
 
 class LoginForm(FlaskForm):
-    """formularz logowania"""
-    userLogin = StringField("Nazwa użytkownika:", validators=[DataRequired()])
-    userPass = PasswordField("Hasło:", validators=[DataRequired()])
-    submit = SubmitField("Zaloguj")
+    """formularza logowania"""
+    userLogin = StringField('Nazwa użytkownika:', validators=[DataRequired()])
+    userPass = PasswordField('Hasło:', validators=[DataRequired()])
+    submit = SubmitField('Zaloguj')
 
+users = {
+    1: {
+        'userLogin': 'lblitek',
+        'userPass': 'Qwerty123!',
+        'fname': 'Łukasz',
+        'lname': 'Blitek'
+    }
+}
 
-users = {1: {"userLogin": "julo", "userPass": "julo", "fname": "julo", "lname": "julo"}}
+def countAverage(subjectValue, termValue):
+    """funkcja obliczająca średnie ocen"""
+    with open('data/grades.json') as gradesFile:
+        grades = json.load(gradesFile)
+        gradesFile.close()
+    sumGrades = 0
+    lenght = 0
+    if subjectValue == "" and termValue == "":
+        for subject, terms in grades.items():
+            for term, categories in terms.items():
+                for category, grades in categories.items():
+                    if category == 'answer' or category == 'quiz' or category == 'test':
+                        for grade in grades:
+                            sumGrades += grade
+                            lenght += 1
+    else:
+        for subject, terms in grades.items():
+            if subject == subjectValue:
+                for term, categories in terms.items():
+                    if term == termValue:
+                        for category, grades in categories.items():
+                            if category == 'answer' or category == 'quiz' or category == 'test':
+                                for grade in grades:
+                                    sumGrades += grade
+                                    lenght += 1
+    if lenght != 0:
+        return round(sumGrades / lenght, 2)
+
+totalAverage = {}
+def yearlyAverage(subjectValue, termValue):
+    with open('data/grades.json', encoding='utf-8') as gradesFile:
+        grades = json.load(gradesFile)
+        gradesFile.close()
+    sumGrades = 0
+    lenght = 0
+    if termValue == '':
+        for subject, terms in grades.items():
+            if subject == subjectValue:
+                for term, categories in terms.items():
+                    for category, grades in categories.items():
+                        if category == 'answer' or category == 'quiz' or category == 'test':
+                            for grade in grades:
+                                sumGrades += grade
+                                lenght += 1
+                                totalAverage[subject] = round(sumGrades / lenght, 2)
+    if lenght != 0:
+        return round(sumGrades / lenght, 2)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='strona glowna', userLogin=session.get("userLogin"), date=date)
+    return render_template('index.html', title='Strona główna', userLogin=session.get('userLogin'), date=date)
 
-
-@app.route('/logIn', methods=["POST", "GET"])
+@app.route('/logIn', methods=['POST', 'GET'])
 def logIn():
-    """obslugiwanie logowania uzytkownikow"""
+    """funkcja obsługująca logowanie użytkowników"""
     login = LoginForm()
     if login.validate_on_submit():
         userLogin = login.userLogin.data
         userPass = login.userPass.data
-        if userLogin == users[1]["userLogin"] and userPass == users[1]["userPass"]:
-            session["userLogin"] = userLogin
-            return redirect("dashboard")
-    return render_template('login.html', title='logowanie', login=login, userLogin=session.get("userLogin"), date=date)
+        if userLogin == users[1]['userLogin'] and userPass == users[1]['userPass']:
+            session['userLogin'] = userLogin
+            return redirect('dashboard')
+    return render_template('login.html', title='Logowanie', login=login, userLogin=session.get('userLogin'), date=date)
 
+@app.route('/logOut')
+def logOut():
+    session.pop('userLogin')
+    return redirect('logIn')
 
-@app.route("/dashboard")
+@app.route('/dashboard')
 def dashboard():
-    with open("data/grades.json") as gradesFile:
+    with open('data/grades.json', encoding='utf-8') as gradesFile:
         grades = json.load(gradesFile)
         gradesFile.close()
-    return render_template("dashboard.html", title="dashboard", userLogin=session.get("userLogin"), grades = grades, date=date)
-
-@app.route("/logOut")
-def logOut():
-    session.pop("userLogin")
-    return redirect("logIn")
+    for subject in grades:
+        yearlyAverage(subject, '')
+    sortedTotalAverage = sorted(totalAverage.items(), key=lambda x: x[1], reverse=True)
+    sortedTotalAverage = dict(sortedTotalAverage)
+    bestTwoSubject = dict(list(sortedTotalAverage.items())[:2])
+    return render_template('dashboard.html', title='Dashboard', userLogin=session.get('userLogin'), date=date, grades=grades, countAverage=countAverage, yearlyAverage=yearlyAverage, bestTwoSubject=bestTwoSubject, sortedTotalAverage=sortedTotalAverage)
 
 if __name__ == '__main__':
     app.run(debug=True)
