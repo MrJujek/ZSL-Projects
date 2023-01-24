239bb40f68e20d4040dfc60b7f55f5980705751b
MrJujek <julian.dworzycki@gmail.com>
2023-01-24 13:02:28 +0000

    js

--- JavaScript2/lekcja10/classes.js
+++ JavaScript2/lekcja10/classes.js
@@ -1,30 +1,127 @@
-class NewGame {
+export class NewGame {
     constructor() {
         this.ctx = document.getElementById("speedway").getContext("2d")
+        this.canvasHeight = document.getElementById("speedway").height
+        this.canvasWidth = document.getElementById("speedway").width
+        this.grassPattern
+        this.pathPattern
+        this.init()
+    };
+    init() {
+        this.makeImagesAndPatterns();
+    };
+    makeImagesAndPatterns() {
+        let grassImg = new Image()
+        grassImg.src = './img/grass.png'
+        grassImg.onload = () => {
+            this.grassPattern = this.ctx.createPattern(grassImg, "repeat");
+
+            let pathImg = new Image()
+            pathImg.src = './img/path.png'
+
+            pathImg.onload = () => {
+                this.pathPattern = this.ctx.createPattern(pathImg, "repeat");
+                this.makeSpeedway();
+            }
+        };
+
+    };
+    makeSpeedway() {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.grassPattern;
+        this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+        this.ctx.closePath();
+
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = "#000";
+        this.ctx.lineWidth = 5;
+        this.ctx.arc(this.canvasWidth / 4 * 1, this.canvasHeight / 2, 200, Math.PI / 2, Math.PI / 2 * 3);
+        this.ctx.arc(this.canvasWidth / 4 * 3, this.canvasHeight / 2, 200, Math.PI / 2 * 3, Math.PI / 2);
+        this.ctx.closePath();
+        this.ctx.fillStyle = this.pathPattern;
+        this.ctx.fill();
+        this.ctx.stroke();
+        this.ctx.closePath();
+
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = "#000";
+        this.ctx.lineWidth = 5;
+        this.ctx.arc(this.canvasWidth / 4 * 1, this.canvasHeight / 2, 50, Math.PI / 2, Math.PI / 2 * 3);
+        this.ctx.arc(this.canvasWidth / 4 * 3, this.canvasHeight / 2, 50, Math.PI / 2 * 3, Math.PI / 2);
+        this.ctx.closePath();
+        this.ctx.fillStyle = this.grassPattern;
+        this.ctx.fill();
+        this.ctx.stroke();
+        this.ctx.closePath();
+
+        let Player1 = new Player(1)
+    }
+}
+
+export class Player {
+    constructor(playerNumber) {
+        this.playerColor
+        this.playerMotorcycle
+        this.ctx = document.getElementById("speedway").getContext("2d")
+
+        switch (playerNumber) {
+            case 1:
+                this.playerX = 100
+                this.playerY = 100
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            default:
+                break;
+        }
+
         this.init()
     }
     init() {
-        //setInterval(() => {
-        console.log("LOG");
-        this.makeSpeedway();
-        //}, 10)
+        this.makePlayer();
+        this.movePlayer();
     }
-    makeSpeedway() {
-        console.log(this.ctx);
-        this.ctx.beginPath();
-        this.ctx.strokeStyle = "#aaf";
-        this.ctx.arc(200, 200, 200, Math.PI / 2, Math.PI / 2 * 3);
-        this.ctx.arc(600, 200, 200, Math.PI / 2 * 3, Math.PI / 2);
-        //this.ctx.lineTo(Math.floor(Math.random() * 400), Math.floor(Math.random() * 400));
-        this.ctx.closePath();
-        this.ctx.stroke();
+    makePlayer() {
+        this.playerMotorcycle = new Image()
+        this.playerMotorcycle.src = './img/motorcycle.png'
+        this.playerMotorcycle.onload = () => {
+
+        }
     }
-    // rysuj: function (x, y) {
-    //     let start = Math.random() * 6
-    //     this.ctx.beginPath();
-    //     this.ctx.strokeStyle = "rgba(255,0,0,0.5)";
-    //     this.ctx.arc(x, y, 50, start, start + Math.PI / 4);
-    //     this.ctx.stroke();
-    //     this.ctx.closePath();
-    // }
+    movePlayer() {
+
+        let alfa, dx, dy
+        setInterval(() => {
+            document.addEventListener('keydown', (e) => {
+                console.log(e.keyCode);
+
+                switch (e.keyCode) {
+                    case 65:
+                        alfa = -10
+                        dx = 10 * Math.sin(alfa)
+                        break;
+
+                    case 67:
+                        alfa = 10
+                        dy = 10 * Math.cos(alfa)
+                        break;
+
+                }
+            })
+
+            this.playerX += dx
+            this.playerY += dy
+
+
+
+        }, 10);
+    }
 }
--- JavaScript2/lekcja10/main.js
+++ JavaScript2/lekcja10/main.js
@@ -1,3 +1,5 @@
+import { NewGame, Player } from './classes.js';
+
 let game
 window.onload = () => {
     game = new NewGame();
