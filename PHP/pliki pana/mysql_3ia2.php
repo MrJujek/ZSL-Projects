1901b3293a21a3a3553d19c3c239a140702646ac
MrJujek <julian.dworzycki@gmail.com>
2023-03-01 18:19:17 +0000

    php

--- PHP/pliki pana/mysql_3ia2.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-$conn=@new mysqli('inf16.tl.krakow.pl','mnowak','mnowak','hurtownia');
-if($conn->connect_errno) die('Nie można połączyć się z serwerem');
-$rs=$conn->query("SELECT nazwa,miasto FROM firmy WHERE miasto='Tarnów'");
-while($rec=$rs->fetch_array())
-	echo $rec['nazwa'],' ',$rec['miasto'],'<br>';
-$rs->close();
-$conn->close();
-?>
