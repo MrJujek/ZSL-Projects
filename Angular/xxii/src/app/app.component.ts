56b34587b52eb215e0038674bd4cc63c11f91493
MrJujek <julian.dworzycki@gmail.com>
2023-06-03 21:45:03 +0200

    angular

--- Angular/xxii/src/app/app.component.ts
+++ Angular/xxii/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import * as xml2js from 'xml2js';
 import { HttpClient } from '@angular/common/http';
+import { parseString } from 'xml2js';
 
 @Component({
   selector: 'app-root',
@@ -12,27 +13,42 @@ export class AppComponent {
   title = 'xxii';
   number: string;
   isDot: boolean = false;
-  specialNumber: boolean = false;
+  specialNumber: boolean = true;
+  src: string = '';
+  imgSrc:string[] = [];
+  showBook: boolean = false;
+  json: any;
+  klik: string = '';
+  lata: string[] = [];
+  year: string = '';
+  yearChoosed: boolean = false;
+  toShow: any = [];
+
 
   constructor(private http: HttpClient) {
-    this.number = '';
+    this.number = '';    
+  }
 
-    this.http.get('https://mendela.pl/ap_kli/czasopisma.xml', { responseType: 'text' }).subscribe(data => {
-      const parser = new DOMParser();
-      const xmlData = parser.parseFromString(data, 'application/xml');
-      console.log(xmlData);
-    });
-
-    // fetch("https://mendela.pl/ap_kli/czasopisma.xml", { mode: 'no-cors' })
-    //   .then(response => response.text())
-    //   .then(data => {
-    //     const parser = new xml2js.Parser({ explicitArray: false });
-    //     parser.parseString(data, (err, result) => {
-    //       console.log(result);
-    //       // console.log(result.czasopisma.zmienne.Atari_Age.src)
-    //     });
-    //   })
-    //   .catch(console.error);
+  ngOnInit() {
+    this.http.get('../assets/czasopisma.xml', { responseType: 'text' })
+      .subscribe(data => {
+        parseString(data, (err, result) => {
+          this.json = result;
+          for (let i = 0; i < Object.keys(result.czasopisma.zmienne[0]).length; i++) {
+            let x = result.czasopisma.zmienne[0][Object.keys(result.czasopisma.zmienne[0])[i]][0].src[0];
+            this.imgSrc.push('http://atarionline.pl/biblioteka/czasopisma/img/'+x);
+            
+            const img = new Image();
+            img.src = 'http://atarionline.pl/biblioteka/czasopisma/img/' + x;
+            img.onload = () => {
+              console.log('Image loaded');
+            };
+          }
+        });
+      },
+      (error) => {
+        console.log(error);
+      });
   }
 
   onInput(event: any) {
@@ -71,6 +87,37 @@ export class AppComponent {
     } else {
       this.number = value.slice(0, value.length - 1);
       event.target.value = this.number;
+    }
+  }
+
+  imgClick(event: MouseEvent) {
+    this.showBook = true;
+    const clickedImg = event.target as HTMLImageElement;
+    const index = this.imgSrc.indexOf(clickedImg.src);
+
+    this.klik = this.json.czasopisma.zmienne[0][Object.keys(this.json.czasopisma.zmienne[0])[index]][0].klik[0];
+
+    this.lata = this.json.czasopisma.lata[0][this.klik][0].split(',');
+  }
+
+  goBack() {
+    this.showBook = false;
+  }
+
+  yearClick(event: MouseEvent) {
+    this.toShow = [];
+    const clickedButton = (event.target as HTMLButtonElement).value;
+    const value = clickedButton;
+    this.year = value;
+    
+    this.yearChoosed = true;
+
+    for (let i = 0; i < Object.keys(this.json.czasopisma[this.klik][0]).length; i++) {
+      if (this.json.czasopisma[this.klik][0][Object.keys(this.json.czasopisma[this.klik][0])[i]][0]['$']['rok'] == this.year) {
+        this.toShow.push(this.json.czasopisma[this.klik][0][Object.keys(this.json.czasopisma[this.klik][0])[i]][0]);
+      } else if (this.year == 'all') {
+        this.toShow.push(this.json.czasopisma[this.klik][0][Object.keys(this.json.czasopisma[this.klik][0])[i]][0]);
+      }
     }
   }
 }
--- Angular/xxii/src/app/clock/clock.component.ts
+++ Angular/xxii/src/app/clock/clock.component.ts
@@ -2,32 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
-  template: '<div>{{ timeString }}</div>'
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.scss']
 })
 export class ClockComponent implements OnInit {
-  timeString: string;
-
-  constructor() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const day = now.getDate().toString().padStart(2, '0');
-    const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const year = now.getFullYear();
-    this.timeString = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
-  }
+  currentTime: number = new Date().getTime();
 
   ngOnInit() {
     setInterval(() => {
-      const now = new Date();
-      const hours = now.getHours().toString().padStart(2, '0');
-      const minutes = now.getMinutes().toString().padStart(2, '0');
-      const seconds = now.getSeconds().toString().padStart(2, '0');
-      const day = now.getDate().toString().padStart(2, '0');
-      const month = (now.getMonth() + 1).toString().padStart(2, '0');
-      const year = now.getFullYear();
-      this.timeString = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+      this.currentTime = new Date().getTime();
     }, 1000);
   }
 }
